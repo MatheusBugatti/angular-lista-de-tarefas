5db697d1b154ed4c2e88f20c7b7e9c592b85559a
matheus <matheus.info@mazatarraf.com.br>
2024-03-22 11:57:17 -0300

    construction(input-lis-item)Inserindo output e input

---
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+
+//interface
+import { IlistItems } from '../../interface/IListItems.interface';
+
+@Component({
+  selector: 'app-input-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-list-item.component.html',
+  styleUrl: './input-list-item.component.scss'
+})
+export class InputListItemComponent {
+  @Input({required : true}) public inputListItems: IlistItems[] = [];
+
+
+  //mudar de pendencia pra concluido
+  @Output() public outputUpdateItemCheckbox = new EventEmitter<{
+    id: string;
+    checked: boolean;
+  }>();
+
+  public updateItemCheckbox(id: string, checked: boolean){
+    return this.outputUpdateItemCheckbox.emit({id, checked});
+  }
+
+  
+  @Output() public outputUpdateItemText = new EventEmitter<{
+    id: string;
+    value: string;
+  }>();
+  
+  public updateItemText(id: string, value: string) {
+    return this.outputUpdateItemText.emit({ id, value });
+  }
+
+  @Output() public outputDeletItemText = new EventEmitter<string>();
+  public deleteItemText(id: string) {
+    return this.outputDeletItemText.emit(id);
+  }
+}
